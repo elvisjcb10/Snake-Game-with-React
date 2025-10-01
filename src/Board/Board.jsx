13bf030a64eca2896c2927c60fcd102fbba45c60
elvisjcb10 <elvisjcb10@gmail.com>
2025-09-30 23:24:41 -0500

    Ahora la serpiente Crece

--- src/Board/Board.jsx
+++ src/Board/Board.jsx
@@ -2,10 +2,9 @@ import { useEffect, useState } from 'react';
 import style from './Board.module.css'
 import Cuadraditos from './Cuadraditos/Cuadraditos';
 const Board=()=>{
-    const [x,setX]=useState(0);
-    const [y,setY]=useState(0);
+
     const [puntuacion,setPuntuacion]=useState(0);
-    const body=[[x,y]];
+    const [body,setBody]=useState([[0,0]]);
     const [manzana,setManzana]=useState([1,3]);
     //Esta funcion hace que luego de comer la manzana se ponga en un lugar aleatorio 
     //afuera del cuerpo de la serpiente
@@ -20,39 +19,36 @@ const Board=()=>{
         }
         //las variables ponemos el estado de manzana para q se genere en otro lado
         setManzana([Mx,My]);
-    }
-
+    }   
     const comerManzana=()=>{
         const [Hx,Hy]=body[0];
         const [Mx,My]=manzana;
         if(Hx===Mx && Hy===My){
             manzanaPosition();
+            setBody([...body,[Mx,My]]);
             setPuntuacion(puntuacion+1);
         }
     }
+    console.log(body);
     useEffect(()=>{
         comerManzana();
         const handleKeydown=(e)=>{
-            if(e.key==="ArrowRight"){
-                setX(x+1);
-                console.log(" se movio a a derecha");
-            }
-            if(e.key==="ArrowLeft"){
-                setX(x-1);
-            }
-            if(e.key==="ArrowDown"){
-                setY(y+1);
-                console.log(" se movio a a derecha");
-            }
-            if(e.key==="ArrowUp"){
-                setY(y-1);
-            }
+            let newHead = [...body[0]];
+
+            if (e.key === "ArrowRight") newHead[0]++;
+            if (e.key === "ArrowLeft") newHead[0]--;
+            if (e.key === "ArrowDown") newHead[1]++;
+            if (e.key === "ArrowUp") newHead[1]--;
+            setBody((prev) => {
+                    const newBody = [newHead, ...prev.slice(0, -1)];
+                    return newBody;
+                });
         };
         window.addEventListener("keydown", handleKeydown);
         return () => {
             window.removeEventListener("keydown", handleKeydown);
         };
-    },[setY,y,setX,x,setPuntuacion])
+    },[setPuntuacion,setBody,body])
     return(
         <div>
             <h2>Score :{puntuacion}</h2>
